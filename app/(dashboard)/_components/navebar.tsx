3c692f052e1aff4333582ca98cdc85efe177324a
vitalik <vitalgreg1988@gmail.com>
2025-01-17 13:49:24 +0200

    search-bar

--- app/(dashboard)/_components/navebar.tsx
+++ app/(dashboard)/_components/navebar.tsx
@@ -1,14 +1,15 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
+import { SearchInput } from "./search-input"
 
 
 
 export const Navbar = () => {
     return (
-        <div className="flex items-center p-5 lg:px-2 bg-green-500 w-full">
+        <div className="flex items-center p-5 lg:px-2 w-full">
             <div className="hidden lg:flex lg:flex-1">
-                {/* <p>to do add search</p> */}
+                <SearchInput />
             </div>
                 <UserButton />
         </div>
--- app/(dashboard)/_components/org-sidebat.tsx
+++ app/(dashboard)/_components/org-sidebat.tsx
@@ -1,10 +1,13 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { OrganizationSwitcher } from "@clerk/nextjs"
+import { LayoutDashboardIcon, StarIcon } from "lucide-react"
 import { Poppins } from "next/font/google"
 import Image from "next/image"
 import Link from "next/link"
+import { useSearchParams } from "next/navigation"
 
 const font = Poppins({
     subsets: ["latin"],
@@ -13,6 +16,10 @@ const font = Poppins({
 })
 
 export const OrgSidebar = () => {
+
+    const searchParams = useSearchParams();
+    const favourite = searchParams.get("favourite");
+
     return (
         <div className="hidden lg:flex flex-col sapce-y-4 w-[206px] pl-5 pt-5">
             <Link href='/'>
@@ -32,9 +39,43 @@ export const OrgSidebar = () => {
                         justifyContent: "center",
                         alignItems: "center",
                         width: "100%",
+                    },
+                    OrganizationSwitcher: {
+                        padding: "6px",
+                        width: "100%",
+                        borderRadius: "8px",
+                        border: "1px solid #e5e7eb",
+                        justifyContent: "space-between",
+                        backgroundColor: "white",
+                     
                     }
                 }
             }}/>
+
+            <div className="space-y-1 w-full">
+                <Button variant={favourite ? "ghost" : "secondary"} asChild size={"lg"} className="font-normal justify-start px-2 w-full">
+                    <Link href='/'>
+                    <LayoutDashboardIcon className="w-4 h-4 mr-2" />
+                    Team Boards
+                    </Link>
+
+                </Button>
+
+                <Button variant={favourite ? "secondary" : "ghost"}  asChild size={"lg"} className="font-normal justify-start px-2 w-full">
+                    <Link href={{
+                        pathname: "/",
+                        query: {
+                            favourite: true
+                        }
+                        
+                    }}>
+                    <StarIcon className="w-4 h-4 mr-2" />
+                    Favourite Boards
+                    </Link>
+
+                </Button>
+
+            </div>
         </div>
 
     )
